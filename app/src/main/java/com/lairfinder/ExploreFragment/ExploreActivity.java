bd87be34238154039da17adaa6a3d7547c77ef77
Nhat Ho <nho@skillz.com>
2017-01-18 22:24:40 -0800

    first commit

---
+++ app/src/main/java/com/lairfinder/ExploreFragment/ExploreActivity.java
@@ -0,0 +1,15 @@
+package com.lairfinder.ExploreFragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lairshare.lairfinder.R;
+
+public class ExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+    }
+}
